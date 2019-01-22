fd07cb7a1b8f0034a160dcda03a802e6bf053299
rmccommon <mccommon@email.arizona.edu>
2019-01-21 22:07:06 -0700

    commented messages.go still need to do main and apifunctions

--- message.go
+++ message.go
@@ -1,46 +1,27 @@
+/*
+**	This handels all the messages that come from or are sent too the javascipt in the application.
+**
+**  This file was created originally to demo astilectron a library that connected go to electron js.
+**	It was rewritten for the purpose of making an app that simulated a self serve soda dispenser that 
+**  uses the Hedera Hashgraph cryptocurrency for making transactions
+*/
+
 package main
 
 import (
 	"encoding/json"
 	"time"
 
-	"github.com/asticode/go-astichartjs"
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
 	"github.com/hashgraph/hedera-sdk-go"
 )
 
-
-
-
 // handleMessages handles messages
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
 	switch m.Name {
-	// case "explore":
-	// 	// Unmarshal payload
-	// 	var path string
-	// 	if len(m.Payload) > 0 {
-	// 		// Unmarshal payload
-	// 		if err = json.Unmarshal(m.Payload, &path); err != nil {
-	// 			payload = err.Error()
-	// 			return
-	// 		}
-	// 	}
 
-	// 	// Explore
-	// 	if payload, err = explore(path); err != nil {
-	// 		payload = err.Error()
-	// 		return
-	// 	}
-
-
-
-
-	// case "onload":
-	// 	if payload, err = explore(); err != nil {
-	// 		payload = err.Error()
-	// 		return
-	// 	}
+	//this is when they hold the button
 	case "tap":
 		// Unmarshall
 		//var time int64
@@ -63,7 +44,7 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 
 // Cost gives all hedera values
 type Cost struct {
-	cost	float64	`json:"cost"`
+	Cost	float64	`json:"cost"`
 }
 
 // Drink represents a liquid to be dispensed
@@ -73,53 +54,16 @@ type Drink struct {
 	flow	float64	`json:"flow"`
 }
 
-// Transaction will store info to send to the Hedera network
-type Transaction struct {
-	drink		string
-	amount		float64
-}
 
-// Exploration represents the results of an exploration
+// Exploration represents a transaction showing both costumer and business balance
 type Exploration struct {
 	Cost		float64				`json:"cost"`
 	Usd			float64				`json:"usd"`
 	C_Balance	float64			`json:"cbal"`
 	B_Balance	float64				`json:"bbal"`
-	Dirs       []Dir              `json:"dirs"`
-	Files      *astichartjs.Chart `json:"files,omitempty"`
-	FilesCount int                `json:"files_count"`
-	FilesSize  string             `json:"files_size"`
-	Path       string             `json:"path"`
 }
 
-// Balance does stuff
-type Balance struct {
-	C_Balance	float64			`json:"cbal"`
-	B_Balance	float64				`json:"bbal"`
-}
-
-// PayloadDir represents a dir payload
-type Dir struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
-}
-
-// func calCost(time int64) (c Cost) {
-// 	// init drink1
-// 	var MtnDew = Drink{
-// 		name: "Mtn Dew",
-// 		price: 0.04,
-// 		flow: 0.1,
-// 	}
-
-// 	c = Cost{
-// 		cost: (float64(time) / 1000.0) * MtnDew.price,
-// 	}
-// 	return
-// }
-
-// explore explores a path.
-// If path is empty, it explores the user's home directory
+// explore does the actual transaction and returns a stuct with the ending balances of the costumer and business
 func explore(timeD int64) (e Exploration, err error) {
 
 	//init drink2
@@ -131,7 +75,7 @@ func explore(timeD int64) (e Exploration, err error) {
 	
 	
 
-
+	// cost is calculated by time multiplied by price since the flow is one floz/sec
 	cost := (float64(timeD) / 1000) * soda.price
 
 	transferAmount(hedera.AccountID{Account: 1001}, hedera.AccountID{Account: 1002}, int64(cost))
@@ -156,15 +100,3 @@ func explore(timeD int64) (e Exploration, err error) {
 	return
 }
 
-// func getBalance() (b Exploration, err error) {
-// 	//Customer
-	
-// 	// Init exploration
-// 	b = Exploration{
-		
-// 		//Dirs: []Dir{},
-// 		//Path: path,
-// 	}
-
-// 	return
-// }
